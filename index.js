b8bc8905165fa69e485082c1e9e1c83cecc9200f
HIMANSHUMISHRA389 <himanshumishra389@gmail.com>
2023-03-20 08:31:21 +0530

    frontend added

--- index.js
+++ index.js
@@ -1,14 +1,28 @@
 const express=require('express');
 const app=express();
+const path=require('path');
 
 
+//using middlewares
+app.use(express.static(path.join(path.resolve(), "public")));
+app.use(express.urlencoded({extended:true}));
 
+
+//Setting up view engine
 app.set('view engine','ejs');
+
+
+//all routes
 app.get('/',(req,res)=>{
-    res.render('index',{name:'Himanshu',title:'Home Page',message:'welcome to my Node.js application!'});
+    res.render('index',{title:'HomePage'});
+})
+app.post('/',(req,res)=>{
+    console.log(req.body);
+   res.send("posted");
+  
 })
 
 app.listen(3000,(req,res)=>{
-    console.log("Nice Work");
+    console.log("Server is up");
 })
 
